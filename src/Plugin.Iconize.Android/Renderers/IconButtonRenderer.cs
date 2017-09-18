7f7409c15e4572c218244de407bb4a8d17606690
Riccardo Marraghini <riccardo.marraghini@gmail.com>
2017-09-18 11:28:20 +0200

    added preprocessor directive for IconButtorRender

--- src/Plugin.Iconize.Android/Renderers/IconButtonRenderer.cs
+++ src/Plugin.Iconize.Android/Renderers/IconButtonRenderer.cs
@@ -4,18 +4,25 @@ using Android.OS;
 using Plugin.Iconize;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+#if USE_FASTRENDERERS
 using ButtonRenderer = Xamarin.Forms.Platform.Android.FastRenderers.ButtonRenderer;
-
+#else
+using ButtonRenderer = Xamarin.Forms.Platform.Android.AppCompat.ButtonRenderer;
+#endif
 [assembly: ExportRenderer(typeof(IconButton), typeof(IconButtonRenderer))]
 namespace Plugin.Iconize
 {
     /// <summary>
     /// Defines the <see cref="IconButtonRenderer" /> renderer.
     /// </summary>
+#if USE_FASTRENDERERS
     /// <seealso cref="Xamarin.Forms.Platform.Android.FastRenderers.ButtonRenderer" />
+#else
+    /// <seealso cref="Xamarin.Forms.Platform.Android.AppCompat.ButtonRenderer" />
+#endif
     public class IconButtonRenderer : ButtonRenderer
     {
-        #region Properties
+#region Properties
 
         /// <summary>
         /// Gets the button.
@@ -25,7 +32,7 @@ namespace Plugin.Iconize
         /// </value>
         private IconButton Button => Element as IconButton;
 
-        #endregion Properties
+#endregion Properties
 
         /// <summary>
         /// Called when [attached to window].
@@ -33,8 +40,11 @@ namespace Plugin.Iconize
         protected override void OnAttachedToWindow()
         {
             base.OnAttachedToWindow();
-
+#if USE_FASTRENDERERS
             TextChanged += OnTextChanged;
+#else
+            Control.TextChanged += OnTextChanged;
+#endif
         }
 
         /// <summary>
@@ -42,8 +52,11 @@ namespace Plugin.Iconize
         /// </summary>
         protected override void OnDetachedFromWindow()
         {
+#if USE_FASTRENDERERS
             TextChanged -= OnTextChanged;
-
+#else
+            Control.TextChanged -= OnTextChanged;
+#endif
             base.OnDetachedFromWindow();
         }
 
@@ -58,7 +71,11 @@ namespace Plugin.Iconize
             if (Button == null)
                 return;
 
+#if USE_FASTRENDERERS
             SetAllCaps(false);
+#else
+            Control.SetAllCaps(false);
+#endif
             if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
             {
                 this.SetBackground(null);
@@ -103,16 +120,29 @@ namespace Plugin.Iconize
         /// </summary>
         private void UpdateText()
         {
+#if USE_FASTRENDERERS
             TextChanged -= OnTextChanged;
+#else
+            Control.TextChanged -= OnTextChanged;
+#endif
 
             var icon = Iconize.FindIconForKey(Button.Text);
             if (icon != null)
             {
+#if USE_FASTRENDERERS
                 Text = $"{icon.Character}";
                 Typeface = Iconize.FindModuleOf(icon).ToTypeface(Context);
+#else
+                Control.Text = $"{icon.Character}";
+                Control.Typeface = Iconize.FindModuleOf(icon).ToTypeface(Context);
+#endif
             }
 
-            TextChanged += OnTextChanged;
+#if USE_FASTRENDERERS
+            TextChanged -= OnTextChanged;
+#else
+            Control.TextChanged -= OnTextChanged;
+#endif
         }
     }
 }
