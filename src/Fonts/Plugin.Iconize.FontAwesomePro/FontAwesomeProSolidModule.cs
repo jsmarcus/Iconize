fcb4fb549f00115fa23c8f356853f7ea0f7f8dd1
Manuel Koetter <koetter.m@buchi.com>
2018-09-13 18:12:05 +0700

    Added FontAwesomePro

---
+++ src/Fonts/Plugin.Iconize.FontAwesomePro/FontAwesomeProSolidModule.cs
@@ -0,0 +1,18 @@
+﻿namespace Plugin.Iconize.Fonts
+{
+    /// <summary>
+    /// Defines the <see cref="FontAwesomeProSolidModule" /> icon module.
+    /// </summary>
+    /// <seealso cref="Plugin.Iconize.IconModule" />
+    public sealed class FontAwesomeProSolidModule : IconModule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FontAwesomeProSolidModule" /> class.
+        /// </summary>
+        public FontAwesomeProSolidModule()
+            : base("Font Awesome 5 Pro Solid", "FontAwesome5ProSolid", "iconize-fontawesome-pro-solid.otf", FontAwesomeProCollection.SolidIcons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
