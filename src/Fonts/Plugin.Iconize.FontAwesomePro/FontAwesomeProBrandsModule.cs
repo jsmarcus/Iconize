449dbd5198a4c07f1dd4a78d4aa00cbd8aae1d7d
Elvin Thudugala <elvin@outlook.co.nz>
2018-11-01 10:35:05 +1300

    Updated to Font Awesome Pro Version 5.4.1

---
+++ src/Fonts/Plugin.Iconize.FontAwesomePro/FontAwesomeProBrandsModule.cs
@@ -0,0 +1,18 @@
+﻿namespace Plugin.Iconize.Fonts
+{
+	/// <summary>
+    /// Defines the <see cref="FontAwesomeProBrandsModule" /> icon module.
+    /// </summary>
+    /// <seealso cref="Plugin.Iconize.IconModule" />
+    public sealed class FontAwesomeProBrandsModule : IconModule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FontAwesomeProBrandsModule" /> class.
+        /// </summary>
+        public FontAwesomeProBrandsModule()
+            : base("Font Awesome 5 Pro Brands", "FontAwesome5ProBrandsRegular", "iconize-fontawesome-pro-brands.ttf", FontAwesomeProCollection.BrandIcons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
