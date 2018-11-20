f05b0f6182d262f82c94f332b7df72d4b58aa9da
Jeremy Marcus <jeremymarcus@msn.com>
2018-11-20 11:10:20 -0500

    Added Jam Icons

---
+++ src/Fonts/Plugin.Iconize.JamIcons/JamIconsModule.cs
@@ -0,0 +1,18 @@
+﻿namespace Plugin.Iconize.Fonts
+{
+    /// <summary>
+    /// Defines the <see cref="JamIconsModule" /> icon module.
+    /// </summary>
+    /// <seealso cref="Plugin.Iconize.IconModule" />
+    public sealed class JamIconsModule : IconModule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JamIconsModule" /> class.
+        /// </summary>
+        public JamIconsModule()
+            : base("Jam-icons", "Jam-icons", "iconize-jam-icons.ttf", JamIconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
