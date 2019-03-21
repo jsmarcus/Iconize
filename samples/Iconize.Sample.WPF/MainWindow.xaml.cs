194d70aa5263802104d2685ceabf2d5a9864ab54
lemmensnick <48796108+lemmensnick@users.noreply.github.com>
2019-03-21 15:02:12 +0100

    Added WPF platform (button, label & image)
    Added a WPF platform (.net 4.7.2) to the samples and main library.
    
    Call "Plugin.Iconize.Iconize.Init();" in the MainWindow.xaml.cs of the WPF project or the icons won't show.
    Add the ttf files to the rout folder and set the build action to "Resource"

---
+++ samples/Iconize.Sample.WPF/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace Iconize.Sample.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Plugin.Iconize.Iconize.Init();
+
+            LoadApplication(new Sample.App());
+        }
+    }
+}
---
+++ src/Plugin.Iconize/Platform/WPF/Renderers/IconLabelRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using Plugin.Iconize;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+[assembly: ExportRenderer(typeof(IconLabel), typeof(IconLabelRenderer))]
+namespace Plugin.Iconize
+{
+    /// <summary>
+    /// Defines the <see cref="IconLabel" /> renderer.
+    /// </summary>
+    /// <seealso cref="Xamarin.Forms.Platform.WPF.LabelRenderer" />
+    public class IconLabelRenderer : LabelRenderer
+    {
+        /// <summary>
+        /// Raises the <see cref="E:ElementChanged" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="ElementChangedEventArgs{Label}" /> instance containing the event data.</param>
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control is null || Element is null)
+                return;
+
+            UpdateText();
+        }
+
+        /// <summary>
+        /// Called when [element property changed].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="PropertyChangedEventArgs" /> instance containing the event data.</param>
+        protected override void OnElementPropertyChanged(Object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Control is null || Element is null)
+                return;
+
+            switch (e.PropertyName)
+            {
+                case nameof(IconLabel.FontSize):
+                case nameof(IconLabel.Text):
+                case nameof(IconLabel.TextColor):
+                    UpdateText();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Updates the text.
+        /// </summary>
+        private void UpdateText()
+        {
+            var icon = Iconize.FindIconForKey(Element.Text);
+            if (!(icon is null))
+            {
+                Control.Text = $"{icon.Character}";
+                Control.FontFamily = Iconize.FindModuleOf(icon).ToFontFamily();
+            }
+        }
+    }
+}
